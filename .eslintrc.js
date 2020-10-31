948c20c7bced92bd685efa2605cfda5947fc5fd5
troy <troycollinsworth@gmail.com>
2020-10-31 09:35:54 -0400

    updated libs, removed babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "extends": "airbnb-base",
-    "plugins": [
-        "import"
-    ],
-    "rules": {
-      "semi": [2, 'never'],
-      "import/no-extraneous-dependencies": ['error', { devDependencies: true }],
-      "max-len": "off",
-      "import/prefer-default-export" : "off",
-      "no-use-before-define": "off",
-      "no-return-assign": ["error", "except-parens"],
-      "no-param-reassign": "off"
-    }
-};
+  extends: 'airbnb-base',
+  plugins: [
+    'import',
+  ],
+  rules: {
+    semi: [2, 'never'],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'max-len': 'off',
+    'import/prefer-default-export': 'off',
+    'no-use-before-define': 'off',
+    'no-return-assign': ['error', 'except-parens'],
+    'no-param-reassign': 'off',
+  },
+}
